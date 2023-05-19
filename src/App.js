dc216f6a558225135dbc623515bfb73585bf6f21
Wes McKnight <wesleymcknight17@gmail.com>
2023-05-19 14:53:09 -0500

    todo list logic

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './styles.css'
+import './styles.css';
 
 function App() {
 
@@ -14,6 +14,25 @@ function App() {
           ...currentTodos,
           { id: crypto.randomUUID(), title: newItem, completed: false },
         ]
+    })
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+      return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo=> todo.id !== id)
     })
   }
 
@@ -33,14 +52,26 @@ function App() {
       </form>
       <h1 className='header'>Todo List</h1>
       <ul className='list'>
+        {todos.length === 0 && "No Todos"}
         {todos.map(todo => {
-          return <li key={todo.id}>
-          <label>
-            <input type='checkbox' checked={todo.completed} />
-            {todo.title}
-          </label>
-          <button className='btn btn-danger'>Delete</button>
-        </li>
+          return (
+            <li key={todo.id}>
+              <label>
+                <input 
+                  type='checkbox' 
+                  onChange={e=> toggleTodo(todo.id, e.target.checked)} 
+                  checked={todo.completed} 
+                />
+                {todo.title}
+              </label>
+              <button 
+                onClick={() => deleteTodo(todo.id)} 
+                className='btn btn-danger'
+              >
+                Delete
+              </button>
+            </li>
+          )
         })}
       </ul>
     </>
